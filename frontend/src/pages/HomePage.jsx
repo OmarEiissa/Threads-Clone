64fbd38bd161c70e750c67cc531b9a5f37d20eab
Omar Eissa <108198032+OmarEiissa@users.noreply.github.com>
2024-08-04 14:47:52 +0300

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -23,10 +23,9 @@ const HomePage = () => {
         const res = await fetch(`${VITE_API_BASE_URL}/api/posts/feed`, {
           credentials: "include" // أضف هذا السطر
         });
-        if (!res.ok) {
-          throw new Error(`Failed to fetch posts: ${res.statusText}`);
-document.write(res.statusText)
-       }
+      //  if (!res.ok) {
+       //   throw new Error(`Failed to fetch 
+      // } 
         const data = await res.json();
         console.log("Feed posts data:", data);
 
