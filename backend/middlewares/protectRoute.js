d720734f5863f48f67eb18a4298df0a07c94323f
Omar Eissa <108198032+OmarEiissa@users.noreply.github.com>
2024-08-04 14:53:39 +0300

    Update protectRoute.js

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -1,22 +1,25 @@
-import User from "../models/userModel.js";
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
 
 const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-
-    if (!token) return res.status(404).json({ message: "Unauthorized" }); //هنا
+    
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     const user = await User.findById(decoded.userId).select("-password");
+    
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
 
     req.user = user;
-
     next();
   } catch (error) {
     res.status(500).json({ message: error.message });
-    console.log("Error in loginUser: ", error.message);
   }
 };
 
