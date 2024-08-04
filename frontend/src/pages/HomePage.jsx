ebfec03096b09ee61db105bcd3f8346c3b8e2317
Omar Eissa <108198032+OmarEiissa@users.noreply.github.com>
2024-08-04 14:25:41 +0300

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -37,6 +37,7 @@ const HomePage = () => {
           setPosts(data);
         } else {
           showToast("Error", "Unexpected data format", "error");
+JSON.stringify(data, null, 2)
         }
 
         if (data.length === 0) {
